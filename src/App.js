b09540240fd04b13b0613e39b965194970952634
amachkel <amachkel@gmail.com>
2022-06-15 14:18:59 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Events from "./pages/Blog";
+import Reviews from "./pages/Reviews";
+import Store from "./pages/Store";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/Reviews" element={<Reviews />} />
+          <Route path="/Store" element={<Store />} />
+          <Route path="/Blog" element={<Blog />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
