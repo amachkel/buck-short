91af11c55e95d9a475e9825eea92edf54ff40b3a
amachkel <amachkel@gmail.com>
2022-07-01 18:37:35 -0400

    add icon button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Logo from "../assets/images/Logos/buck-short.png";
+// import { Link as Scroll } from "react-scroll";
 import { styled } from "@mui/system";
-import { CssBaseline } from "@mui/material";
-import Box from "@mui/material/Box";
+import {
+  CssBaseline,
+  Typography,
+  Box,
+  Collapse,
+  IconButton,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
 const HomeStyled = styled("div")({
-  minHeight: "100%",
+  minHeight: "100vh",
   backgroundImage: `url(${Logo})`,
   backgroundRepeat: "no-repeat",
   backgroundPosition: "center",
@@ -12,21 +20,44 @@ const HomeStyled = styled("div")({
 });
 
 export default function Home() {
+  // const [checked, setChecked] = useState(false);
+  // useEffect(() => {
+  //   setChecked(true);
+  // }, []);
   return (
     <HomeStyled>
       <CssBaseline />
       <Box
         sx={{
-          height: "100vh",
-          backgroundColor: "rgba(0, 0, 0, 0.2)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          minHeight: "100vh",
         }}
       >
-        A Buck Short Productions is a film production company based out of
-        Hampton, Georgia. Writer and director, Richard Tanner, began A Buck
-        Short after an unfortunate run-in with a cold-hearted bitch. Though the
-        journey was long and painful, they emerged from the toxic waste as film
-        makers, a strong team, and kick-ass kung-fu masters!
+        {/* <Scroll to="welcome" smooth={true}> */}
+        <IconButton>
+          <ExpandMoreIcon
+            sx={{
+              fontSize: "8rem",
+              color: "#000000",
+            }}
+          />
+        </IconButton>
+        {/* </Scroll> */}
       </Box>
     </HomeStyled>
   );
+}
+
+{
+  /* <Box sx={{ width: 300, height: 300 }}>
+        <Typography color={"#000000"}>
+          A Buck Short Productions is a film production company based out of
+          Hampton, Georgia. Writer and director, Richard Tanner, began A Buck
+          Short after an unfortunate run-in with a cold-hearted bitch. Though
+          the journey was long and painful, they emerged from the toxic waste as
+          film makers, a strong team, and kick-ass kung-fu masters!
+        </Typography>
+      </Box> */
 }
